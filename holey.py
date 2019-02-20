194f3d20581cef0a51cbe26523b3bfcb0d45152c
Sebastian Krautwurst <raverjay@gmail.com>
2019-02-20 18:09:13 +0100

    expansion added

--- holey.py
+++ holey.py
@@ -29,10 +29,16 @@ class holeyRect:
 
     dims = None
     ranges = None
+
     volume = None
 
     def __init__(self, ranges):
         self.dims = ranges.shape[0]
+        self.ranges = ranges
+        self.volume = self.get_volume()
+
+    def update_ranges(self, ranges):
+        assert self.dims == ranges.shape[0]
         self.ranges = ranges
         self.volume = self.get_volume()
 
@@ -47,7 +53,7 @@ class holeyRect:
                 ret += '\t...\n'
                 break
             ret += f'\t( {self.ranges[dim][0]}, \t{self.ranges[dim][1]} )\n'
-        ret += '>\n'
+        ret += '>'
         return ret
 
 
@@ -55,7 +61,8 @@ class holeyRect:
 class holey:
 
 
-    supported_input_data_types = ['doc2vec']
+    supported_input_data_types = ['numpy', 'doc2vec']
+    supported_rect_expansion_strategies = ['randmax']
 
     data = None
     samples = None
@@ -72,32 +79,44 @@ class holey:
     projs_lens = []
 
 
-    def __init__(self, input_file):
+    def __init__(self, input_data, input_data_type='doc2vec'):
 
-        self.load_data(input_file)
+        if input_data_type not in self.supported_input_data_types:
+            error(f'Input data type not supported: {input_data_type}')
+
+        self.load_data(input_data, input_data_type)
 
 
-    def load_data(self, input_file, input_data_type='doc2vec'):
+    def load_data(self, input_data, input_data_type='doc2vec'):
 
         if input_data_type not in self.supported_input_data_types:
             error(f'Input data type not supported: {input_data_type}')
 
         # data types
         if input_data_type == 'doc2vec':
-            self.load_data_doc2vec(input_file)
-
+            self.load_data_doc2vec(input_data)
+        elif input_data_type == 'numpy':
+            self.load_data_numpy(input_data)
         else:
             error(f'FATAL: No loading function implemented for data type: {input_data_type}')
 
 
-    def load_data_doc2vec(self, input_file):
+    def load_data_numpy(self, input_data):
+
+        assert len(input_data.shape) == 2
+        self.samples, self.dims = input_data.shape
+        self.data = input_data.copy()
+        log(f'Loaded numpy array data: {self.samples} vectors of length {self.dims}.')
+
+
+    def load_data_doc2vec(self, input_data):
 
         from gensim.models import Doc2Vec
 
         try:
-            model = Doc2Vec.load(input_file)
+            model = Doc2Vec.load(input_data)
         except:
-            error(f'Failed to load doc2vec model from file: {input_file}')
+            error(f'Failed to load doc2vec model from file: {input_data}')
 
         vec_count = model.docvecs.count
         vec_len = model.docvecs.vector_size
@@ -105,7 +124,7 @@ class holey:
         self.dims = vec_len
 
         self.data = model.docvecs.vectors_docs
-        log(f'Loaded doc2vec data: {vec_count} vectors of length {vec_len}.')
+        log(f'Loaded doc2vec data: {self.samples} vectors of length {self.dims}.')
 
 
     def normalize_data(self):
@@ -151,6 +170,34 @@ class holey:
 #############
 
 
+    def check_if_point_in_rectangle(self, rect, point):
+
+        for dim in range(rect.dims):
+            if point[dim] < rect.ranges[dim][0] or point[dim] > rect.ranges[dim][1]:
+                return False
+        # contained in all dimensions
+        return True
+
+
+    def check_if_point_in_ranges(self, ranges, point):
+
+        for dim in range(ranges.shape[0]):
+            if point[dim] < ranges[dim][0] or point[dim] > ranges[dim][1]:
+                return False
+        # contained in all dimensions
+        return True
+
+
+    def check_if_no_data_in_ranges(self, ranges):
+
+        for point in self.data:
+            if self.check_if_point_in_ranges(ranges, point):
+                # found a point inside the ranges
+                return False
+        # no points inside the ranges
+        return True
+
+
     def get_surrounding_edges(self, value, dim):
         for i, edge in enumerate(self.projs[dim]):
             if edge > value:
@@ -158,7 +205,123 @@ class holey:
                 return self.projs[dim][i-1], edge
 
 
-    def make_rectangle_monte_carlo(self):
+
+    def expand_rectangle(self, rect, dimension, direction, amount='maximal'):
+        '''Expand a rectangle along one dimension in one direction by some amount.
+        Stop expanding at data points (rectangle would be non-empty).
+        If amount == 'maximal', expand until a data point is encountered.
+        '''
+
+        assert dimension < rect.dims
+        up = None
+        if direction == '+':
+            up = True
+        elif direction == '-':
+            up = False
+        else:
+            error(f'Rectangle expansion direction must be either + or - .')
+        
+        if amount != 'maximal':
+            error('Specific amount expansion NYI')
+            # if amount <= 0 or amount > 1:
+            #     error(f'Rectangle expansion amount must be in (0, 1]  - given: {amount}')
+
+        ranges = rect.ranges
+        done = False
+        if up:
+            edge = ranges[dimension][1]
+
+            # find next step on orthogonal projection
+            for val in self.projs[dimension]:
+                # skip lower values
+                if val <= edge:
+                    continue
+
+                # val is outside rectangle, expand
+                ranges[dimension][1] = val
+
+                # check if expansion is ok
+                for point in self.data:
+                    
+                    if self.check_if_point_in_ranges(ranges, point):
+                        # expanded too far, revert
+                        ranges[dimension][1] = edge
+                        done = True
+                        break
+
+                if done:
+                    break
+
+        else:
+            edge = ranges[dimension][0]
+
+            # find next step on orthogonal projection
+            for val in self.projs[dimension][::-1]:
+                # skip higher values
+                if val >= edge:
+                    continue
+
+                # val is outside rectangle, expand
+                ranges[dimension][0] = val
+
+                # check if expansion is ok
+                for point in self.data:
+                    
+                    if self.check_if_point_in_ranges(ranges, point):
+                        # expanded too far, revert
+                        ranges[dimension][0] = edge
+                        done = True
+                        break
+
+                if done:
+                    break
+        
+
+        rect.update_ranges(ranges)
+        # log(f'Expanded to {rect.ranges[dimension]} in direction {direction} on dimension {dimension}.')
+        return rect
+
+
+    def maximize_rectangle_randmax(self, rect):
+        '''Expand dimensions in random order, each maximally'''
+
+        # log(f'>>> Starting expansion with rect: {rect}')
+
+        # i+dims are dummies for negative directions
+        dimorder = np.random.permutation(self.dims*2)
+        for dim in dimorder:
+            if dim < self.dims:
+                rect = self.expand_rectangle(rect, dim, '+')
+            else:
+                rect = self.expand_rectangle(rect, dim-self.dims, '-')
+
+        # log(f'Ended expansion with rect: {rect}\n#############')
+
+        return rect
+
+
+
+    def maximize_rectangle(self, rect, expansion_strategy):
+        '''Expand rectangle to be maximal
+
+        Supported expansion strategies:
+        - randmax:  random order of dims, each maximally
+        '''
+
+        if expansion_strategy not in self.supported_rect_expansion_strategies:
+            error(f'Unknown rectangle expansion strategy: {expansion_strategy}')
+
+        if expansion_strategy == 'randmax':
+            return self.maximize_rectangle_randmax(rect)       
+
+        else:
+            error(f'Rectangle expansion strategy NYI: {expansion_strategy}')
+
+
+    def make_rectangle_monte_carlo(self, expansion_strategy='randmax'):
+
+        if expansion_strategy not in self.supported_rect_expansion_strategies:
+            error(f'Unknown rectangle expansion strategy: {expansion_strategy}')
 
         # throw a random dart at the board    >--~-  ((o))
         dart = np.random.rand(self.dims)
@@ -168,10 +331,11 @@ class holey:
         for dim in range(self.dims):
             ranges[dim] = self.get_surrounding_edges(dart[dim], dim)
         rect = holeyRect(ranges)
-        
+
         # expand
+        rect = self.maximize_rectangle(rect, expansion_strategy)
 
-
+        assert self.check_if_point_in_rectangle(rect, dart)
 
         return rect
             
@@ -188,6 +352,8 @@ class holey:
         # init
         if size_threshold == 'default_1_over_n':
             size_threshold = 1/self.dims
+        elif size_threshold < 0 or size_threshold >= 1:
+            error(f'Rectangle size threshold must be in [0, 1)  - given: {size_threshold}')
 
         max_volume_found = 0
         current_tries = 0
@@ -224,29 +390,37 @@ if __name__ == '__main__':
 
     log('Started holey test run ...')
 
-    # load model
-    model = '/home/ya86gul/scripts/holey/nanotext/nanotext/data/embedding.genomes.model'
     log('Loading model ...')
-    holey = holey(model)
+    # load model
+    # model = '/home/ya86gul/scripts/holey/nanotext/nanotext/data/embedding.genomes.model'
+    # holey = holey(model)
 
-    # subset model
-    holey.data = holey.data[:100]
+    # # subset model
+    # holey.data = holey.data[:100]
 
-    print(holey.data[:2])
+    ho = holey(np.random.randn(100, 10), 'numpy')
+    # holey = holey(np.array([3,5,7,9,5,4,7,5,3,4,9,6,4,2,4], dtype=float).reshape((5,3)), 'numpy')
+
+    print(ho.data[:5])
     log('Normalizing data ...')
 
-    holey.normalize_data()
-    print(holey.data[:2])
+    ho.normalize_data()
+    print(ho.data[:5])
 
     # log('Unnormalizing data ...')
     # holey.unnormalize_data()
-    # print(holey.data[:2])
+    # print(holey.data[:5])
 
-    holey.get_orthogonal_projections()
-    print(holey.projs_lens)
+    ho.get_orthogonal_projections()
+    print(ho.projs_lens)
 
 
-    rects = holey.generate_rectangles(10)
-    print(rects)
+    rects = ho.generate_rectangles(num_tries=100)
+
+    # check them again
+    for rect in rects:
+        print(rect)
+        empty = ho.check_if_no_data_in_ranges(rect.ranges)
+        print(f'Rectangle is empty: {empty}')
 
     log('Done.')
